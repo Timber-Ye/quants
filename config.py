64917049912539278dce8cae858dd649dac3d26e
lxc-debug <3072514597@qq.com>
2023-04-15 11:40:58 +0800

    参数提取

---
+++ config.py
@@ -0,0 +1,17 @@
+import argparse
+
+parser=argparse.ArgumentParser(description='config info')
+parser.add_argument('--api_key',default='8da5d3ad-7554-424e-a1c2-bfcead40dcdd')
+parser.add_argument('--secret_key',default='E086EDAC65DF1E55DA72CA2483327CA2')
+parser.add_argument('--passphrase',default='lxc123@LXC')
+parser.add_argument('--Permissions',default='Read/Withdraw/Trade')
+parser.add_argument('--use_server_time',default=False,type=bool)
+parser.add_argument('--flag',default='1')
+
+parser.add_argument('--stop_loss_multiplier',default='0.98',type=float,help='止损倍数')
+parser.add_argument('--max_holding_period',default='20',type=int,help='最长持仓天数')
+parser.add_argument('--stop_loss_period',default='5',type=int,help='止损触发期')
+parser.add_argument('--trailing_stop_multiplier',default='0.98',type=float,help='移动止损倍数')
+parser.add_argument('--trailing_stop_period',default='5',type=int,help='移动止损触发期')
+
+args=parser.parse_args()
--- use_api.py
+++ use_api.py
@@ -4,21 +4,25 @@ import okx.Funding as Funding
 import okx.Trade as Trade
 import pandas as pd
 import numpy as np
+from config import args
 
 
-def get_personal_info(use_server_time=False, flag='1'):
-    api_key = "8da5d3ad-7554-424e-a1c2-bfcead40dcdd"
-    secret_key = "E086EDAC65DF1E55DA72CA2483327CA2"
-    passphrase = "lxc123@LXC"
-    Permissions = "Read/Withdraw/Trade"
+def get_personal_info():
+    api_key = args.api_key
+    secret_key = args.secret_key
+    passphrase = args.passphrase
+    Permissions = args.Permissions
+    use_server_time=args.use_server_time
+    flag=args.flag
 
     return api_key, secret_key, passphrase, use_server_time, flag
 
 
-def trade(instId='BTC-USDT', side='buy', sz=1, use_server_time=False, flag='1'):
+def trade(instId='BTC-USDT', side='buy', sz=1):
     # 三个参数，币种，买卖，数量
-    per_info = get_personal_info(use_server_time=use_server_time, flag=flag)
+    per_info = get_personal_info()
     tradeAPI = Trade.TradeAPI(*per_info)
+    print(*per_info,type(per_info[3]))
     result = tradeAPI.place_order(
         instId=instId, tdMode='cash', side=side, ordType='market', sz=sz)
     if result['code'] == '1':
@@ -29,7 +33,7 @@ def trade(instId='BTC-USDT', side='buy', sz=1, use_server_time=False, flag='1'):
 
 def get_k(flag='1', instId='BTC-USDT', bar='15m'):
     # 获取k线图数据，可以根据返回结果进行k线图绘制
-    marketDataAPI = MarketData.MarketAPI(flag=flag)
+    marketDataAPI = MarketData.MarketAPI(flag=args.flag)
     result = marketDataAPI.get_history_candlesticks(instId=instId, bar=bar)
     df = pd.DataFrame(result['data'])
     k_data = df.iloc[:, 1:5]
@@ -38,9 +42,9 @@ def get_k(flag='1', instId='BTC-USDT', bar='15m'):
     return k_data
 
 
-def get_balance(use_server_time=False, flag='1', ccy='USDT'):
+def get_balance( ccy='USDT'):
     # 获取账户USDT余额
-    per_info = get_personal_info(use_server_time=use_server_time, flag=flag)
+    per_info = get_personal_info()
     accountAPI = Account.AccountAPI(*per_info)
 
     result = accountAPI.get_account_balance(ccy=ccy)
@@ -50,25 +54,13 @@ def get_balance(use_server_time=False, flag='1', ccy='USDT'):
                   detail['availBal'])  # 显示币种，币种余额，可用余额
 
 
-def ma_stoploss(k_data, stop_loss_multiplier=0.98, max_holding_period=20, stop_loss_period=5, trailing_stop_multiplier=0.98, trailing_stop_period=5):
-    """使用ma_stoploss策略
-
-    Arguments:
-        k_data -- k线图数据
-
-    Keyword Arguments:
-        stop_loss_multiplier -- 止损倍数 (default: {0.98})
-        max_holding_period -- 最长持仓天数 (default: {20})
-        stop_loss_period -- 止损触发期 (default: {5})
-        trailing_stop_multiplier -- 移动止损倍数 (default: {0.98})
-        trailing_stop_period -- 移动止损触发期 (default: {5})
-    """
+def ma_stoploss(k_data):
     position = 0
     pass    #现在需要判断读入数据是否是同一个时间的data，如果不同，则调用小面函数进行操作
 
 
-def ma_sloss_step(k_data, position, max_holding_period,trailing_stop_multiplier):
-    global stop_loss,trailing_high,trailing_low,holding_period
+def ma_sloss_step(k_data):
+    global stop_loss,trailing_high,trailing_low,holding_period,position
     # 计算布林线
     k_data['MA20'] = k_data['price'].rolling(window=20).mean()
     k_data['std'] = k_data['price'].rolling(window=20).std()
